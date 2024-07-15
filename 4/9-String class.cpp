380b6210b7c37581f3116ec30e55b8da0d3d1251
Arham Apon <arhamaponutsho@gmail.com>
2024-07-15 14:38:18 +0600

    completed 4(almost)

---
+++ 4/9-String class.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string s; //this is dynamic
+  string b;
+  getline(cin,b); // this will also get the spaces
+  cout<<b<<endl;
+  getline(cin,b,',');
+  cout<<b;
+
+
+}
